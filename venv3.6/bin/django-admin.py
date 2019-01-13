5f96138d8e14160c8fe42e551ad7cbd039197a08
James Sung <sjkskywalker@korea.ac.kr>
2019-01-12 21:42:41 -0800

    Installed Django v1.11.0

---
+++ venv3.6/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/skywalker/Desktop/JK/github/my/blog-example/venv3.6/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
